dd73965342d8d1fcb22aa11d39cd5f416842a935
jagdish-git <jagadishbal080@gmail.com>
2022-03-11 09:03:10 +0530

    added new view function in views.py

--- practoapp2/views.py
+++ practoapp2/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def view1(request):
     return HttpResponse('<h1>Welocme</h1>')
+
+def view2(request):
+    return HttpResponse('<h1>Welocme from Jagdish</h1>')
